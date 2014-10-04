f3e8e6990a87e52347e9f697afc82be540ee8164
Richard Rivas <i.ricard26@gmail.com>
2014-10-03 22:06:29 -0500

    Migracion proyecto
    Creacion del menu de navegacion full.

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+	<html lang="es">
+	<head>
+			<meta charset="utf-8">
+			<meta name="description" content="Different styles of fashion Temptations Marilin">
+			<meta name="keywords" content="fashion, style, Temptations, Marilin">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Tentaciones Marilin</title>
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/style.css">
+			<link rel="stylesheet" href="css/normalize.css">
+			<script src="js/jquery-1.11.1.min.js"></script>
+			<script src="js/bootstrap.js"></script>
+			<script src="js/functions.js"></script>
+	</head>
+	<body>
+		<!-- HEADER -->
+		<header>
+			<nav class="navbar navbar-default">
+			<!-- LOGO -->
+				<ul class="nav navbar-nav pull-left">
+					<li>
+						<img src="img/logop.png" width="50" alt="Logo Tentaciones Marilin" title="Tentaciones Marilin" data-placement="right">
+					</li>
+				</ul>
+			<!-- BTN NAV -->
+				<div class="navbar-header">
+					<button id="btnMenu" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navheader">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+			<!-- CONTENT NAVBAR -->
+				<div class="collapse navbar-collapse" id="navheader">
+					<ul class="nav navbar-nav pull-right">
+						<li class="box-index" ><a href="#">Inicio</a></li>
+						<li class="box-sections"><a href="#sections">Secciones</a></li>
+						<li class="box-contact"><a href="#contact">Contáctenos</a></li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+		<div class="banner">
+			<div class="banner-text">
+				<h1>Tentaciones <span><img src="img/logop.png" alt="Logo pequeño" width="30"></span> Marilin</h1>
+				<h3>Moda a tu gusto</h3>
+			</div>
+			<video width="100%" loop autoplay muted>
+				<source src="video/VID-20140927-WA0000.mp4" type="video/mp4">
+			</video>
+			<video width="100%" loop autoplay muted>
+				<source src="video/VID-20140927-WA0000.mp4" type="video/mp4">
+			</video>
+		</div>
+	</body>
+			<script>
+			$('a, img').tooltip();
+			</script>
+</html>
---
+++ js/functions.js
@@ -0,0 +1,13 @@
+$(function(){
+	//validar la posicion del scroll
+	if (window.scrollTop != 0) {
+		$('.navbar-toggle').addClass('fixed');
+	};
+	//manejador del boton menu
+	$('.navbar-toggle').on('click',function(){
+		$('html, body').animate({
+			scrollTop: 0
+		},500);
+		$('.navbar-toggle').toggleClass('fixed');
+	});
+});
